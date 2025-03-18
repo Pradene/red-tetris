0497a3e13b69836a6667461835dceb7dd849b7f2
Pradene <liam.pradene@gmail.com>
2025-03-18 15:45:08 +0100

    Changing color theme

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React from "react";
 
-import { CellState, Cell } from "./Cell"
+import { CellState, Cell } from "./Cell";
+
+import styles from "./Board.module.css";
 
 interface BoardProps {
 	cols: number,
@@ -37,36 +39,22 @@ export const Board: React.FC<BoardProps> = ({cols, rows, board}) => {
 
 
 	const getColorForCell = (type: CellState) => {
-		// Define color mappings for each theme
-		const colorMapping: Record<CellState, string> = {
-			"J": "#6F1D1B",
-			"L": "#F7C548",
-			"O": "#2B50AA",
-			"T": "#2B50AA",
-			"I": "darkcyan",
-			"Z": "darkred",
-			"S": "darkgreen",
-			"X": "red",
-			"0": "transparent",
-		}
-
-		// Return color based on the current theme
-		return colorMapping[type] || "transparent"
+		return `var(--color-${type.toUpperCase()})`;
 	}
 
 	return (
-		<div style={{
-			height: "100%",
-			display: "grid",
-			margin: "auto",
-			gridTemplateColumns: `repeat(${cols}, minmax(0, auto))`,
-			gridTemplateRows: `repeat(${rows}, minmax(0, auto))`,
-		}}>
+		<div
+			className={styles.board}
+			style={{
+				"--cols": cols,
+				"--rows": rows,
+			} as React.CSSProperties}
+		>
 		{standardizedBoard.map((row, rowIndex) =>
 			row.map((cell, colIndex) => (
 				<Cell
-				key={`${rowIndex * cols + colIndex}`}
-				color={getColorForCell(cell)} // Pass the color dynamically based on the cell state
+					key={`${rowIndex * cols + colIndex}`}
+					color={getColorForCell(cell)} // Pass the color dynamically based on the cell state
 				/>
 			))
 		)}
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import LogoutButton from "./LogoutButton"
 
 import styles from "./Nav.module.css"
@@ -6,7 +7,9 @@ import styles from "./Nav.module.css"
 const Nav: React.FC = () => {
     return (
         <nav className={styles.nav}>
-            <h1 className={styles.title}>Tetris</h1>
+            <Link to="/" style={{textDecoration: "none"}}>
+                <h1 className={styles.title}>Tetris</h1>
+            </Link>
             <LogoutButton />
         </nav>
     )
